1702e2c4b3eb03b8e8c5cf41d16bf34281312eac
jtmaliel <jtmaliel@332f6c18-f343-0410-b13e-1102dc9ed7da>
2008-03-27 17:39:08 +0000

    Initial acf parser commit.  Code is not currently commented, I will comment it in the next commit.

---
+++ acfparser/acfparser.lisp
@@ -0,0 +1,66 @@
+(in-package "ACL2")
+(include-book "io-utilities" :dir :teachpacks)
+(set-state-ok t)
+(include-book "list-utilities" :dir :teachpacks)
+
+#|----------------------reading file -> list of strings---------------|#
+(defun parse-lines (line)
+  (let* ((info (tokens (list #\tab) line))
+         (cmds (chrs->str (car info))))
+    cmds))
+
+(defun parse-list (l)
+  (if (consp l)
+      (cons (parse-lines (car l)) (parse-list (cdr l)))
+      nil))
+
+(defun readfile (file state)
+  (mv-let (str error state)
+          (file->string file state)
+          (if error
+              (mv error state)
+              (parse-list (tokens (list #\( #\) #\return #\Newline) (str->chrs str))))))
+#|-------------------END file->list of strings------------------------|#
+
+(defstructure acf
+  (output       (:assert (string-listp output)))
+  (files        (:assert (string-listp files)))
+  (commands     (:assert (string-listp commands))))
+
+(defun keywords-output (word)
+  (let ((keylist '("put-signal" "display-signal")))
+        (if (member-equal word keylist)
+            t
+            nil)))
+
+(defun get-vars (vars str)
+  (if (consp vars)
+      (cons (cons (car vars) (car str)) (get-vars (cdr vars) (cdr str)))
+      nil))
+
+(defun get-cmds (str vars)
+  (if (not (equal (car str) vars))
+      (cons (car str) (get-cmds (cdr str) vars))
+      nil))
+  
+(defun line-parser(str)
+  (let* ((output (if (keywords-output (caar str))
+                     (car str)
+                     nil))
+         (file-vars (if (not (eq output nil))
+                    (caddr str)
+                    (cadr str)))
+         (vars-list (get-vars (reverse file-vars) (reverse str)))
+         (commands (get-cmds (nthcdr (len file-vars) (reverse str)) file-vars)))
+    (acf output vars-list commands)))
+
+(defun line->words(line)
+  (if (consp line)
+      (remove nil (cons (words (car line)) (line->words (cdr line))))
+      nil))
+
+(defun parser (file state)
+  (let* ((str (line->words (readfile file state)))
+         (struct (line-parser str)))
+    struct))
+    
