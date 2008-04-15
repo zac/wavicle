aebff507456b28bb99ba53141b426a2d9ee8b65b
masonwillinger <masonwillinger@332f6c18-f343-0410-b13e-1102dc9ed7da>
2008-04-15 21:04:15 +0000

    New parser stuff

--- trunk/acfparser.lisp
+++ trunk/acfparser.lisp
@@ -1,8 +1,7 @@
 (in-package "ACL2")
 ;(include-book "io-utilities" :dir :teachpacks)
 
-
-#|----------------------reading file -> list of strings---------------|#
+#|------------------------Reading the ACF file--------------------------------|#
 (defun parse-lines (line)
   (let* ((info (tokens (list #\tab) line))
          (cmds (chrs->str (car info))))
@@ -18,31 +17,43 @@
           (file->string file state)
           (if error
               (mv error state)
-              (mv (parse-list (tokens (list #\( #\) #\Newline) (str->chrs line))) state))))
-#|-------------------END file->list of strings------------------------|#
+              (mv (parse-list (tokens (list #\( #\) #\Newline) 
+                                      (str->chrs line))) state))))
+#|--------------------------------End of this code----------------------------|#
 
+#|------------------------Required Structures and Keywords--------------------|#
+;Structure that contains the required information to run the program.
 (defstructure acf
   (output       (:assert (string-listp output)))
   (commands     (:assert (true-listp commands))))
 
+;Function that checks for output types.
 (defun keywords-output (word)
   (let ((keylist '("put-signal" "display-signal")))
         (if (member-equal word keylist)
             t
             nil)))
+;Function that checks effects words.
+(defun cmd-keys (word)
+  (let ((keylist '("boost" "fuzz" "delay" "overdub" "echo" "fade-in" "fade-out" 
+                           "cut" "chipmunk" "reverse" "filter")))
+        (if (member-equal word keylist)
+            t
+            nil)))
+#|--------------------------------End of this code----------------------------|#
 
+
+#|------------------------------------Get-Vars--------------------------------|#
+;Function that gets the list of file variables from the
+;parameters of the lamda function.
 (defun get-vars (vars str)
   (if (consp vars)
       (cons (cons (car vars) (car str)) (get-vars (cdr vars) (cdr str)))
       nil))
+#|--------------------------------End of this code----------------------------|#
 
-(defun get-cmds (str vars)
-  (if (consp str)
-      (if (not (equal (car str) vars))
-          (cons (car str) (get-cmds (cdr str) vars))
-          nil)
-      nil))
-
+#|---------------------Replaces file variables with actual file names---------|#
+;Helper function for replace-vars-helper
 (defun replace-variable (word vars-list file-vars)
   (if (consp vars-list)
       (if (member-equal word file-vars)
@@ -51,43 +62,85 @@
               (replace-variable word (cdr vars-list) file-vars))
           word)
       nil))
-  
+
+;Helper function for replace-vars 
 (defun replace-vars-helper (cmd vars-list file-vars)
   (if (consp cmd)
-      (cons (replace-variable (car cmd) vars-list file-vars) (replace-vars-helper (cdr cmd) vars-list file-vars))
+      (cons (replace-variable (car cmd) vars-list file-vars) 
+            (replace-vars-helper (cdr cmd) vars-list file-vars))
       nil))
 
+;Function that initially receives the list of commands,
+;list of variables, and list of file names corresponding
+;with file names
 (defun replace-vars (cmds vars-list file-vars)
   (if (consp cmds)
-      (cons (replace-vars-helper (car cmds) vars-list file-vars) (replace-vars (cdr cmds) vars-list file-vars))
+      (cons (replace-vars-helper (car cmds) vars-list file-vars) 
+            (replace-vars (cdr cmds) vars-list file-vars))
       nil))
+#|--------------------------------End of this code----------------------------|#
 
+#|-------------------------Get last element in a list-------------------------|#
+;Function that gets the last element in the list.
+(defun get-last (xs)
+  (if (endp xs)
+      nil
+      (car (nthcdr (- (length xs) 1) xs))))
+#|--------------------------------End of this code----------------------------|#
+
+#|---------------------------------Verify Commands----------------------------|#
+;Checks if the commands in the list of lamda functions have overdub.
+;If it does contain overdub, makes sure the commands are properly arranged.
+(defun get-cmds (str)
+  (if (consp str)
+      (if (string-equal "overdub" (caar str))
+          (if (equal (len (car str)) 1)
+              (cons (cons (caar str) (get-last str)) 
+                    (get-cmds (butlast (cdr str) 1)))
+              (cons (car str) (get-cmds (cdr str))))
+          (cons (car str) (get-cmds (cdr str))))
+      nil))
+#|--------------------------------End of this code----------------------------|#
+
+#|--------------------------------Create ACF Structure------------------------|#
+;Returns an acf structure that contains the output type
+;and the list of commands
 (defun line-parser(str)
-  (let* ((output (if (keywords-output (caar str))
+  (if (equal str nil)
+      "No data available to parse"
+      (let* ((output (if (keywords-output (caar str))
                      (car str)
                      nil))
          (file-vars (if (not (eq output nil))
                     (caddr str)
                     (cadr str)))
          (vars-list (get-vars (reverse file-vars) (reverse str)))
-         (commands (get-cmds (nthcdr (len file-vars) (reverse str)) file-vars))
-         (newcmds (replace-vars commands vars-list file-vars)))
-    (acf output newcmds)))
+         (cmds-hlpr (nthcdr (len file-vars) (reverse (nthcdr 3 str))))
+         (commands (reverse (get-cmds (reverse cmds-hlpr))))
+         (newcmds (if (equal (len commands) 0)
+                      (if (string-equal "display-signal" (car output))
+                          (cdar vars-list)
+                          nil)
+                     (replace-vars commands vars-list file-vars))))
+   (acf output newcmds))))
+#|--------------------------------End of this code----------------------------|#
 
+#|--------------------------Driver to parse acf file--------------------------|#
+;Function that takes out empty spaces in the original string
+;that is received after reading the file.
 (defun line->words(line)
   (if (consp line)
       (remove nil (cons (words (car line)) (line->words (cdr line))))
       nil))
 
+;Main function that returns the acf structure containing
+;the required information to run the program.
 (defun parser (file state)
   (mv-let (str state)
           (readfile file state)
           (mv (line-parser (line->words str)) state)))
+#|--------------------------------End of this code----------------------------|#
 
-(defun get-last (xs)
-  (if (endp xs)
-      nil
-      (car (nthcdr (- (length xs) 1) xs))))
     
 (defun acf-function-h (commands wav state)
   (if (endp commands)
@@ -109,9 +162,11 @@
                                    (acf-function-h (cdr commands) wav state)
                                    wav2)) state))
               ((string-equal arg1 "overdub")
-               (mv (overdub (mv-let (wav2 state) (read-wav arg2 state) wav2) (mv-let (wav3 state)
-                                   (acf-function-h (cdr commands) wav state)
-                                   wav3)) state))
+               (if (not (cmd-keys arg2))
+                   (mv (overdub (mv-let (wav2 state) (read-wav arg2 state) wav2)
+                                (mv-let (wav3 state) (acf-function-h (cdr commands) wav state) wav3)) state)
+                   (mv (overdub (mv-let (wav4 state) (acf-function-h (cons (butlast arg2 1) (butlast (get-last arg2) 1)) (get-last (get-last arg2)) state) wav4)
+                                (mv-let (wav5 state) (acf-function-h (cdr commands) wav state) wav5)) state)))
               ((string-equal arg1 "echo")
                (mv (echo (str->rat arg2) (str->rat arg3) (mv-let (wav2 state)
                                    (acf-function-h (cdr commands) wav state)
