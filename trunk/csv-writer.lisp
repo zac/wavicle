3434fc5e4a6648839a097058ae94d4adb877e162
jtmaliel <jtmaliel@332f6c18-f343-0410-b13e-1102dc9ed7da>
2008-04-16 19:24:26 +0000

    Takes in a list of integers and outputs a csv file. Initial commit..

---
+++ trunk/csv-writer.lisp
@@ -0,0 +1,33 @@
+(in-package "ACL2")
+(include-book "io-utilities" :dir :teachpacks)
+(include-book "world" :dir :teachpacks)
+(include-book "list-utilities" :dir :teachpacks)
+
+#|---------------------Convert Rational numbers to string----------------------|#
+;converts a list of rational numbers to a list of comma seperated strings
+(defun convert-to-string (str)
+  (if (consp str)
+      (concatenate 'string (rat->str (car str) 5) 
+                   (concatenate 'string "," (convert-to-string (cdr str))))
+      ""))
+
+;removes the last comma in the list of strings
+(defun remove-last-comma (str)
+  (subseq str 0 (- (length str) 1)))
+
+;combine the two above functions.
+(defun get-csv-list (ratlist)
+  (let* ((str-comma (convert-to-string ratlist))
+         (str       (remove-last-comma str-comma)))
+    str))
+#|--------------------------------End of this code----------------------------|#
+
+
+#|------------------------Writing the CSV file--------------------------------|#
+(defun write-csv (file str state)
+  (mv-let (error state)
+          (string-list->file file (list (get-csv-list str)) state)
+          (if error
+              (mv error state)
+              (mv "Success" state))))
+#|--------------------------------End of this code----------------------------|#
