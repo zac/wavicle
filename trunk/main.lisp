896102c5d945ce3009c66a63ae6f37616e344200
masonwillinger <masonwillinger@332f6c18-f343-0410-b13e-1102dc9ed7da>
2008-04-22 00:50:52 +0000

    More comments to acf-parser and main

--- trunk/main.lisp
+++ trunk/main.lisp
@@ -7,6 +7,11 @@
 (include-book "csv-writer")
 (include-book "acfparser")
 
+; (main file state)
+; Runs wavicle using the acf specified.
+;
+; file - The path to the acf
+; state - The state variable
 (defun main (file state)
   (mv-let (acf state)
           (parser file state)
