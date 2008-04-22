3dff61eae071b910ad840621cc9db9858d1286df
masonwillinger <masonwillinger@332f6c18-f343-0410-b13e-1102dc9ed7da>
2008-04-22 00:47:32 +0000

    Added comments to acf-parser.lisp

--- acfparser.lisp
+++ acfparser.lisp
@@ -76,7 +76,10 @@
 #|--------------------------------End of this code----------------------------|#
 
 #|-------------------------Get last element in a list-------------------------|#
-;Function that gets the last element in the list.
+; (get-last)
+; Function that gets the last element in the list.
+;
+; xs - The list from which you want to retrieve the last element
 (defun get-last (xs)
   (if (endp xs)
       nil
@@ -138,8 +141,13 @@
 
 #|-------------------------------Parser/Operator Integration------------------|#
 
-;Helper function for acf-function that translates what is returned from
-;the parser into function calls that will alter a wav file
+; (acf-function-h)
+; Helper function for acf-function that translates what is returned from
+; the parser into function calls that will alter a wav file
+;
+; commands - The list of commands to be performed on the wav file
+; wav - The wav struture representing the wav file to be modified
+; state - The state variable
 (defun acf-function-h (commands wav state)
   (if (endp commands)
       (mv wav state)
@@ -161,11 +169,17 @@
                                    wav2)) state))
               ((string-equal arg1 "overdub")
                (if (not (cmd-keys arg2))
-                   (mv (overdub (mv-let (wav3 state) (acf-function-h (cdr commands) wav state) wav3)
-                                (mv-let (wav2 state) (read-wav arg2 state) wav2)) state)
-                   (mv (overdub (mv-let (wav6 state) (acf-function-h (cdr commands) wav state) wav6)
-                                (mv-let (wav4 state) (acf-function-h (list (butlast (cdr args) 1))
-                                (mv-let (wav5 state) (read-wav (get-last args) state) wav5) state) wav4)) state)))
+                   (mv (overdub (mv-let (wav3 state)
+                                (acf-function-h (cdr commands) wav state) wav3)
+                                (mv-let (wav2 state)
+                                (read-wav arg2 state) wav2)) state)
+                   (mv (overdub (mv-let (wav6 state) 
+                                (acf-function-h (cdr commands) wav state) wav6)
+                                (mv-let (wav4 state)
+                                (acf-function-h (list (butlast (cdr args) 1))
+                                (mv-let (wav5 state)
+                                (read-wav (get-last args) state) wav5)
+                                state) wav4)) state)))
               ((string-equal arg1 "echo")
                (mv (echo (str->rat arg2) (str->rat arg3) (mv-let (wav2 state)
                                    (acf-function-h (cdr commands) wav state)
@@ -195,9 +209,13 @@
                            (mv-let (wav2 state)
                                    (acf-function-h (cdr commands) wav state)
                                    wav2)) state))))))
-              
-;Sets up the necessary conditions for acf-function-h to correctly call
-;functions to alter a wav file.
+
+; (acf-function)
+; Sets up the necessary conditions for acf-function-h to correctly call
+; functions to alter a wav file.
+;
+; acf - The parsed ACF from (parser acf state)
+; state - the state variable
 (defun acf-function (acf state)
   (let* ((output (acf-output acf))
          (action (car output))
@@ -219,3 +237,4 @@
                        (mv "Bad csv filename" state)
                    (let ((temp (display-wave wav)))
                          (write-csv file (wav-file-data wav) state)))))))))
+#|-----------------------------End of this Code-------------------------------|#
