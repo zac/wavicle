09964fdf84358169a4d170e60b25e633a1ce9aca
zacwhite <zacwhite@332f6c18-f343-0410-b13e-1102dc9ed7da>
2008-04-22 03:37:30 +0000

    tests.lisp doesn't work, but trying to add random wav generation.

--- trunk/tests.lisp
+++ trunk/tests.lisp
@@ -68,7 +68,6 @@
 (defgenerator random-normalized-rat (power)
   (1 (/ (random-int-between 0 (- (expt 2 power) 1)) (- (expt 2 power) 1))))
 
-
 (defun wav->list (wav)
   (list (wav-file-chunk-id wav) ;chunk-id
         (wav-file-chunk-size wav) ;chunk-size
@@ -101,35 +100,26 @@
             (nth 12 lst)
             (nth 13 lst)))
 
-(defun generate-wav-list (num-channels sample-rate bits-per-sample
-                                       num-samples random-lst)
-  (list '(82 73 70 70)
-        2684
-        '(87 65 86 69)
-        '(102 109 116 32)
-        16
-        1
-        num-channels
-        sample-rate
-        (* sample-rate num-channels bits-per-sample)
-        (* num-channels (/ bits-per-sample 8))
-        bits-per-sample
-        '(100 97 116 97)
-        (* (* num-samples num-channels) (/ bits-per-sample 8))
-        random-lst))
-        
 
-;(defgenerator wav-gen (max-length)
-;  (1 (generate-wav-list (random-int-between 1 2)
-;                        (random-int-between 8000 96000)
-;                        (* 8 (random-int-between 1 4))
-;                        (random-int-between 10 max-length)
-;                        (random-list-of (random-normalized-rat (- bits-per-sample 1))
-;                                        data-length))))
+(defgenerator wav-gen (max-length)
+  (1 (random-list -1
+                  2684
+                  -1
+                  -1
+                  16
+                  1
+                  (random-int-between 1 2)
+                  (random-int-between 8000 96000)
+                  -1
+                  -1
+                  (* (random-int-between 1 4) 8)
+                  -1
+                  -1
+                  (random-list-of (random-normalized-rat 16) (random-int-between 0 max-length)))))
 
-;(defproperty wav-test 1
-;  ((wav (wav-gen 1000) t))
-;  (equal wav (wav->list (boost 1 (list->wav wav)))))
+(defproperty wav-test 1
+  ((wav (wav-gen 1000) t))
+  (equal wav (wav->list (boost 1 (list->wav wav)))))
 
 ;filter tests
 
