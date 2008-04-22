94e8aa72ada54dd7352ada407df8f03044bad1c8
zacwhite <zacwhite@332f6c18-f343-0410-b13e-1102dc9ed7da>
2008-04-22 22:04:54 +0000

    Added all the tests from acfparsertests.lisp

--- trunk/tests.lisp
+++ trunk/tests.lisp
@@ -1,9 +1,113 @@
 (in-package "ACL2")
 (include-book "doublecheck" :dir :teachpacks)
-(include-book "structures")
-(include-book "operators")
-(include-book "wav")
-(include-book "filter")
+(include-book "main")
+
+;----------------Helper functions required to run the tests------------------
+;sample acf files to read.
+(defun sample1 ()
+  "(put-signal output.wav
+                ((lambda (f input1 input2)
+                       (overdub (filter f input1)
+                                (chipmunk 2 input2)))
+             (get-filter test.flt)
+             (get-signal samples/voice1.wav)
+             (get-signal samples/voice2.wav)))")
+
+;sample acf file to test nested overdub arguments
+(defun sample2 ()
+  "(put-signal output.wav
+                ((lambda (f input1 input2)
+                       (overdub (filter f input1)
+                                (chipmunk 2 (fade-out 2 input2))))
+             (get-filter test.flt)
+             (get-signal samples/voice1.wav)
+             (get-signal samples/voice2.wav)))")
+
+;sample acf file to test filepath in quotes
+(defun sample3 ()
+  "(put-signal \"output.wav\"
+                ((lambda (f input1 input2)
+                       (overdub (filter f input1)
+                                (chipmunk 2 input2)))
+             (get-filter test.flt)
+             (get-signal samples/voice 1.wav)
+             (get-signal samples/voice 2.wav)))")
+
+;sample acf file to test empty lamdba function
+(defun sample4 ()
+  "(display-signal output.wav
+                ((lambda (input1))
+             (get-signal samples/voice1.wav))))")
+;function that will take a string and run the parser on it
+;
+;str - acf file in a string form.
+(defun runparser (str)
+  (let ((sample (parse-list (tokens (list #\( #\) #\Newline) 
+                                      (str->chrs str)))))
+    (line-parser (line->words sample))))
+
+
+;-----------------------ACF Parser Unit Tests--------------------------------
+;function that returns t or nil depending on the
+;similarities of the expected and actual results.
+;from running the sample1.
+
+(defun test-sample1 ()
+  (let ((expected (acf '("put-signal" "output.wav") 
+                       '(("filter" "test.flt" "samples/voice1.wav") 
+                         ("overdub" "chipmunk" "2" "samples/voice2.wav"))))
+        (actual (runparser (sample1 ))))
+    (if (equal expected actual)
+        t
+        nil)))
+
+;function that returns t or nil depending on the
+;similarities of the expected and actual results.
+;from running the sample2.
+(defun test-sample2 ()
+  (let ((expected (acf '("put-signal" "output.wav") 
+                       '(("fade-out" "2" "samples/voice2.wav") 
+                         ("chipmunk" "2") 
+                         ("overdub" "filter" "test.flt" "samples/voice1.wav"))))
+        (actual (runparser (sample2 ))))
+    (if (equal expected actual)
+        t
+        nil)))
+
+;function that returns t or nil depending on the
+;similarities of the expected and actual results.
+;from running the sample3.
+(defun test-sample3 ()
+  (let ((expected (acf '("put-signal" "output.wav") 
+                       '(("filter" "test.flt" "samples/voice 1.wav") 
+                       ("overdub" "chipmunk" "2" "samples/voice 2.wav"))))
+        (actual (runparser (sample3 ))))
+    (if (equal expected actual)
+        t
+        nil)))
+
+;function that returns t or nil depending on the
+;similarities of the expected and actual results.
+;from running the sample4.
+(defun test-sample4 ()
+  (let ((expected (acf '("display-signal" "output.wav") 
+                       '("get-signal" "samples/voice1.wav")))
+        (actual (runparser (sample4 ))))
+    (if (equal expected actual)
+        t
+        nil)))
+
+; (test-all)
+; Runs all unit tests and reports the first failure.
+(defun test-all ()
+  (cond ((not (test-sample1)) "Test 1 FAILS")
+        ((not (test-sample2)) "Test 2 FAILS")
+        ((not (test-sample3)) "Test 3 FAILS")
+        ((not (test-sample4)) "Test 4 FAILS")
+        (t "ALL TESTS PASS")))
+
+; Runs all tests.
+(test-all)
 
 ;; Theorems.
 
